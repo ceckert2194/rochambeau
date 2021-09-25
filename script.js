e53366f3bfbeca7458bc7a7da47ef9262cfb025c
ceckert2194 <ceckert2194@gmail.com>
2021-09-25 13:50:04 -0400

    javascript WORKING thank god

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let choices = ['rock', 'paper', 'scissors']
 var playerScore = 0;
 var computerScore = 0;
-const btn = document.querySelector('#play');
-btn.addEventListener('click', function(e) {
-    e.target.style.background = 'blue';
-});
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorBtn = document.querySelector('#scissors');
 
 function computerPlay() {
     choice_index = Math.floor(Math.random() * choices.length)
     return choices[choice_index]
 }
+
+// let computerChoice = choices[Math.floor(Math.random() * choices.length)]
 
 function playerPlay() {
     const playerChoice = window.prompt("Rock, Paper, or Scissors: ").toLowerCase();
@@ -18,9 +19,9 @@ function playerPlay() {
 
 function playRound(playerChoice, computerChoice) {
 
-    playerChoice = playerPlay()
+    // playerChoice = playerPlay()
     computerChoice = computerPlay()
-
+    
     if (playerChoice == 'rock' && computerChoice == 'paper') {
         computerScore += 1;
         return (`You lost, ${computerChoice} beats ${playerChoice}!`)
@@ -51,15 +52,57 @@ function playRound(playerChoice, computerChoice) {
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i+1}: ` + playRound())
-    }
+    // for (let i = 0; i < 5; i++) {
+    //     console.log(`Round ${i+1}: ` + playRound())
+    // }
 
-    if (playerScore > computerScore) {
-        return (`You won the game! You won ${playerScore} rounds and the computer won ${computerScore}.`)
-    } else if (playerScore < computerScore) {
-        return (`You lost the game! You won ${playerScore} rounds and the computer won ${computerScore}.`)
+    // if (playerScore >= 5) {
+    //     return ('Game over, you won!')
+    // } else if (computerScore >= 5) {
+    //     return ('Game over, you lost!')
+    // }
+
+    if (playerScore < 5 && computerScore < 5) {
+        void(0);
     } else {
-        return (`The game was a draw! You and the computer both won ${playerScore} rounds.`)
+        if (playerScore > computerScore) {
+            alert('Win!')
+            return (`You won the game! You won ${playerScore} rounds and the computer won ${computerScore}.`)
+        } else if (playerScore < computerScore) {
+            alert('Lost!')
+            return (`You lost the game! You won ${playerScore} rounds and the computer won ${computerScore}.`)
+        } else {
+            return (`The game was a draw! You and the computer both won ${playerScore} rounds.`)
+        }
     }
 }
+
+//     if (playerScore > computerScore) {
+//         return (`You won the game! You won ${playerScore} rounds and the computer won ${computerScore}.`)
+//     } else if (playerScore < computerScore) {
+//         return (`You lost the game! You won ${playerScore} rounds and the computer won ${computerScore}.`)
+//     } else {
+//         return (`The game was a draw! You and the computer both won ${playerScore} rounds.`)
+//     }
+// }
+
+rockBtn.addEventListener('click', () => {
+    console.log(playRound(playerChoice='rock'));
+    document.querySelector('#results').textContent = playerScore;
+    document.querySelector('#computer-results').textContent = computerScore;
+    game();
+});
+
+paperBtn.addEventListener('click', () => {
+    console.log(playRound(playerChoice='paper'));
+    document.querySelector('#results').textContent = playerScore;
+    document.querySelector('#computer-results').textContent = computerScore;
+    game();
+});
+
+scissorBtn.addEventListener('click', () => {
+    console.log(playRound(playerChoice='scissors'));
+    document.querySelector('#results').textContent = playerScore;
+    document.querySelector('#computer-results').textContent = computerScore;
+    game();
+});
